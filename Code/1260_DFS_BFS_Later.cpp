031bac3b482066b3cc2db37eb8ca096940ac9baa
chohj1111 <sigyebang@gmail.com>
2020-07-17 23:47:09 +0900

    200717

--- Code/1260_DFS_BFS_Later.cpp
+++ Code/1260_DFS_BFS_Later.cpp
@@ -1,45 +1,69 @@
-#include <iostream>
+#include <cstdio>
+#include <stack>
+#include <algorithm>
+#include <cstring>
 #include <vector>
 #include <queue>
 using namespace std;
-
-vector<int> graph[1001];
+vector<int> a[1001];
 bool check[1001];
-
-void DFS(int v) {
-	check[v] = true; 
-	cout << v << " ";
-	for (int i = 0; i < graph[v].size(); i++) {
-		if(!check[graph[v][i]])
-			DFS(graph[v][i]);
-	}
-}
-
-void BFS(int v) {
-	queue<int>q;
-	q.push(v);
-	check[v] = true;
-	cout << v << " ";
-	while (!q.empty()) {
-		int x = q.front();
-		q.pop();
-		for (int i = 0; i < graph[x].size(); i++) {
-			int y = graph[x][i];
-			if (!check[y]) {
-				check[y] = true;
-				cout << y << " ";
-				q.push(y);
+void dfs(int node) {
+	stack<pair<int, int>> s;
+	s.push(make_pair(node, 0));
+	check[node] = true;
+	printf("%d ", node);
+	while (!s.empty()) {
+		int node = s.top().first;
+		int start = s.top().second;
+		s.pop();
+		for (int i = start; i < a[node].size(); i++) {
+			// 노드에 연결된 다른 노드들 확인
+			int next = a[node][i];
+			if (check[next] == false) {
+				printf("%d ", next);
+				check[next] = true;
+				// dfs & stack 특성 이용 next에 연결된 첫 노드를 다음 탐색 target으로 
+				s.push(make_pair(node, i + 1));
+				s.push(make_pair(next, 0));
+				//next에 연결된 노드가 우선이므로 break
+				break;
 			}
 		}
 	}
 }
-
-int main() {
-	int n, m, v;
-	cin >> n >> m >> v;
-	for (int i = 0; i < m; i++) {
-		int from, int to;
-		cin >> from >> to;
-		graph
+void bfs(int start) {
+	queue<int> q;
+	check[start] = true;
+	q.push(start);
+	while (!q.empty()) {
+		int node = q.front();
+		q.pop();
+		printf("%d ", node);
+		for (int i = 0; i < a[node].size(); i++) {
+			int next = a[node][i];
+			if (check[next] == false) {
+				check[next] = true;
+				q.push(next);
+			}
+		}
 	}
+}
+int main() {
+	int n, m, start;
+	scanf("%d %d %d", &n, &m, &start);
+	for (int i = 0; i < m; i++) {
+		int u, v;
+		scanf("%d %d", &u, &v);
+		a[u].push_back(v);
+		a[v].push_back(u);
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(a[i].begin(), a[i].end());
+	}
+	dfs(start);
+	printf("\n");
+	memset(check, false, sizeof(check));
+	bfs(start);
+	printf("\n");
+	return 0;
 }
