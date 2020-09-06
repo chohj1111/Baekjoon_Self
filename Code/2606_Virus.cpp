d09f03faab16346f865de5c99007cd34bed81369
chohj1111 <sigyebang@gmail.com>
2020-09-06 21:01:35 +0900

    200906

--- Code/2606_Virus.cpp
+++ Code/2606_Virus.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
+vector<vector<int>> network;
+vector<bool> visit;
+int ans = 0;
+void dfs(int v) {
+	ans++;
+	visit[v] = true;
+	for (int i = 0; i < network[v].size(); i++) {
+		if(!visit[network[v][i]])
+			dfs(network[v][i]);
+	}
+	return;
+}
 int main() {
+	int com_num;
+	cin >> com_num;
+	 network.resize(com_num+1);
+	 visit.resize(com_num + 1);
+
+	int net_num;
+	cin >> net_num;
+	for (int i = 0; i < net_num; i++) {
+		int start, end;
+		cin >> start >> end;
+		network[start].push_back(end);
+		network[end].push_back(start);
+	}
+	dfs(1);
+	cout << ans - 1 << endl;
 	return 0;
 }
